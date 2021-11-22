7103fa389e6c3c686585b2645e955f3cd19f9fe3
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2021-11-22 09:46:29 +0300

    add new task

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const operations = {
-  AND: (a, b) => a && b,
-  OR: (a, b) => a || b,
-  XOR: (a, b) => a !== b,
-};
-
-const logicalCalc = (array, op) => array.reduce(operations[op]);
+function correctTail(bod, tail) {
+  return bod[bod.length - 1] === tail;
+}
