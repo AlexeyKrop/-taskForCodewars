8087506d20bace206a8e30e85edcaee1754dd806
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2021-11-24 13:02:13 +0300

    N-th Power

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-function getDrinkByProfession(param) {
-  param = param.toLowerCase();
-
-  switch (param) {
-    case "jabroni":
-      return "Patron Tequila";
-    case "school counselor":
-      return "Anything with Alcohol";
-    case "programmer":
-      return "Hipster Craft Beer";
-    case "bike gang member":
-      return "Moonshine";
-    case "politician":
-      return "Your tax dollars";
-    case "rapper":
-      return "Cristal";
-    default:
-      return "Beer";
-  }
+function index(array, n) {
+  return array.length > n ? array[n] ** n : -1;
 }
