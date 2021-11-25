2eb237b5fe80c6f63d7ac675e6e54578f6fe04ff
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2021-11-25 08:46:47 +0300

    How much water do I need?

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function index(array, n) {
-  return array.length > n ? array[n] ** n : -1;
+function howMuchWater(W, L, C) {
+  if (C > 2 * L) return "Too much clothes";
+  if (C < L) return "Not enough clothes";
+  return +(W * 1.1 ** (C - L)).toFixed(2);
 }
