efc276a429b85f6b94fab3ac1eafe6b458416c86
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2021-12-15 13:50:21 +0300

    add new task

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-const elevator = (left, right, call) =>
-  Math.abs(call - left) < Math.abs(call - right) ? "left" : "right";
+const isPrime = (n) => {
+  if (n > 0) {
+    if (n === 1 || n === 0) {
+      return false;
+    } else {
+      for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+  } else {
+    return false;
+  }
+};
