cc378add7349fbb0e532b8d3be1063a6a0f5585c
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2021-12-27 08:49:10 +0300

    new task

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function shortcut(name) {
-  return;
-}
-console.log(shortcut("Patrick Feenan"));
+const areaOrPerimeter = function (l, w) {
+  if (l === w) {
+    return l * w;
+  } else {
+    return 2 * (l + w);
+  }
+};
