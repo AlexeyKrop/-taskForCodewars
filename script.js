729bd1dd7b09b2fa1c8b93620495bb5bb1bb1f20
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2021-12-29 10:42:43 +0300

    add new kata

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function alternateCase(s) {
-  return s
-    .split("")
-    .map((a) => (a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()))
-    .join("");
+function isIsogram(str) {
+  let strArr = str.toLowerCase();
+  for (let i = 0; i < strArr.length; ++i) {
+    for (let j = i + 1; j < strArr.length; ++j) {
+      if (strArr[i] === strArr[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
+isIsogram("moo");
