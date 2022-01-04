e37ccd59a91dc8800c8521508c961415b60644fc
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-01-04 12:34:09 +0300

    add new kata

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-const sumin = (n) => (n * (n + 1) * (2 * n + 1)) / 6;
-const sumax = (n) => (n * (n + 1) * (4 * n - 1)) / 6;
-const sumsum = (n) => n * n * (n + 1);
+function largestPairSum(numbers) {
+  return numbers
+    .sort((a, b) => a - b)
+    .slice(-2)
+    .reduce((a, b) => a + b, 0);
+}
