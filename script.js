86bb6e5412f0b351851e9eedcaed4ac3891c441c
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-01-05 10:40:20 +0300

    add new kata

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function bitsBattle(numbers) {
-  const even = numbers
-    .filter((v) => v % 2 === 0)
-    .map((v) => v.toString(2))
-    .join("")
-    .replace(/1/g, "").length;
-  const odd = numbers
-    .filter((v) => v % 2 !== 0)
-    .map((v) => v.toString(2))
-    .join("")
-    .replace(/0/g, "").length;
-  return even > odd ? "evens win" : even < odd ? "odds win" : "tie";
+function descendingOrder(n) {
+  return +(n + "").split("").sort().reverse().join("");
 }
