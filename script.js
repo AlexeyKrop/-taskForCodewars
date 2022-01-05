1e6e9c1410c8cdddf8f65797ffbbcc17cb2e4f26
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-01-05 10:39:25 +0300

    add new kata

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function isTriangle(a, b, c) {
-  let max = Math.max(a, b, c);
-  let sum = a + b + c;
-  return sum - max > max;
+function bitsBattle(numbers) {
+  const even = numbers
+    .filter((v) => v % 2 === 0)
+    .map((v) => v.toString(2))
+    .join("")
+    .replace(/1/g, "").length;
+  const odd = numbers
+    .filter((v) => v % 2 !== 0)
+    .map((v) => v.toString(2))
+    .join("")
+    .replace(/0/g, "").length;
+  return even > odd ? "evens win" : even < odd ? "odds win" : "tie";
 }
