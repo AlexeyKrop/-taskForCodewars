e7328394a33d5d5425a8ca33894bf4d8eb4e5a25
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-01-05 10:37:44 +0300

    add new kata

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function isFibo(a, b, f) {
-  let arr = [a, b];
-  for (let i = 1; i < f + 10; i++) {
-    arr.push(arr[i - 1] + arr[i]);
-  }
-  return arr.includes(f);
+function elevatorDistance(array) {
+  return array
+    .map((v, i, arr) => arr[i] - arr[i - 1])
+    .filter((v) => !isNaN(v))
+    .reduce((a, b) => a + Math.abs(b), 0);
 }
