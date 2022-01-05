d2a2bd5a9ac599e50e16dd494b69ed9def29daca
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-01-05 10:41:02 +0300

    add new kata

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-function descendingOrder(n) {
-  return +(n + "").split("").sort().reverse().join("");
+function encode(str) {
+  return str
+    .split("")
+    .map((v) => (/[a-z]/gi.test(v) ? v.toLowerCase().charCodeAt() - 96 : v))
+    .join("");
 }
