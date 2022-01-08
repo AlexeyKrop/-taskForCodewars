125d4dcda31e095c5485451639f18a269fbdee1b
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-01-08 17:35:50 +0300

    add new task

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-function rpsls(pl1, pl2) {
-  let win = "Draw!";
-  let arr = [
-    "scissors cuts paper",
-    "paper covers rock",
-    "rock crushes lizard",
-    "lizard poisons spock",
-    "spock smashes scissors",
-    "scissors decapitates lizard",
-    "lizard eats paper",
-    "paper disproves spock",
-    "spock vaporizes rock",
-    "rock crushes scissors",
-  ].map((v) => v.split` `);
-  arr.map((v) =>
-    v[0] === pl1 && v[2] === pl2
-      ? (win = "Player 1")
-      : v[0] === pl2 && v[2] === pl1
-      ? (win = "Player 2")
-      : v
-  );
-  return win !== "Draw!" ? `${win} Won!` : win;
+function finalGrade(exam, projects) {
+  if (exam > 90 || projects > 10) return 100;
+  if ((exam > 75) & (projects >= 5)) return 90;
+  if ((exam > 50) & (projects >= 2)) return 75;
+  return 0;
 }
