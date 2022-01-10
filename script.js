3fef6f53ed04fae9b0a738d2fb4e1041141fbf14
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-01-10 12:59:55 +0300

    dd new kata

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function finalGrade(exam, projects) {
-  if (exam > 90 || projects > 10) return 100;
-  if ((exam > 75) & (projects >= 5)) return 90;
-  if ((exam > 50) & (projects >= 2)) return 75;
-  return 0;
+function pairZeros(arr) {
+  let zero = 0;
+  return arr
+    .map((v) => {
+      if (v === 0) {
+        if (v === 0) {
+          zero++;
+        }
+        if (v === 0 && zero % 2 === 0) return (v = null);
+      }
+      return v;
+    })
+    .filter((v) => v !== null);
 }
