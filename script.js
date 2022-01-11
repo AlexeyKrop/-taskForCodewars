cef3fb0ec2402a249c4cf8ab23524ad207dbf5ea
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-01-11 09:49:42 +0300

    add new kata

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-function rpsls(pl1, pl2) {
-  let win = "Draw!";
-  let arr = [
-    "scissors cuts paper",
-    "paper covers rock",
-    "rock crushes lizard",
-    "lizard poisons spock",
-    "spock smashes scissors",
-    "scissors decapitates lizard",
-    "lizard eats paper",
-    "paper disproves spock",
-    "spock vaporizes rock",
-    "rock crushes scissors",
-  ].map((v) => v.split` `);
-  arr.map((v) =>
-    v[0] === pl1 && v[2] === pl2
-      ? (win = "Player 1")
-      : v[0] === pl2 && v[2] === pl1
-      ? (win = "Player 2")
-      : v
-  );
-  return win !== "Draw!" ? `${win} Won!` : win;
+function sixToast(num) {
+  return Math.abs(num - 6);
 }
