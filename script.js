c5d4dea09a35198b9e92b5116694e972f633ccbb
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-01-12 09:28:39 +0300

    add new kata

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-function sixToast(num) {
-  return Math.abs(num - 6);
+function oddest(a) {
+  let arr = a.map((x) => [...(x >>> 0).toString(2)].reverse());
+  return a[arr.reduce((i, a, b) => (a > arr[i] ? b : i), 0)];
 }
