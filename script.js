27ba64ff5161f3ce71b626d3015f71794a9e5293
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-01-13 08:42:42 +0300

    add new kata

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-const babySharkLyrics = () => {
-  const fish = [
-    "Baby shark",
-    "Mommy shark",
-    "Daddy shark",
-    "Grandma shark",
-    "Grandpa shark",
-    "Let's go hunt",
-  ];
-  let lyrics = "";
-  let s = " doo".repeat(6);
-  fish.map((f) => {
-    lyrics += `${f},${s}\n`.repeat(3) + `${f}!\n`;
-  });
-  lyrics += "Run away,…";
-  return lyrics;
-};
+function rpsls(pl1, pl2) {
+  let win = "Draw!";
+  let arr = [
+    "scissors cuts paper",
+    "paper covers rock",
+    "rock crushes lizard",
+    "lizard poisons spock",
+    "spock smashes scissors",
+    "scissors decapitates lizard",
+    "lizard eats paper",
+    "paper disproves spock",
+    "spock vaporizes rock",
+    "rock crushes scissors",
+  ].map((v) => v.split` `);
+  arr.map((v) =>
+    v[0] === pl1 && v[2] === pl2
+      ? (win = "Player 1")
+      : v[0] === pl2 && v[2] === pl1
+      ? (win = "Player 2")
+      : v
+  );
+  return win !== "Draw!" ? `${win} Won!` : win;
+}
