c657bb63e7543141770938bcf384fc32bb9c4233
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-01-13 08:42:06 +0300

    add new kata

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-function paul(x) {
-  let n = 0;
-  x.map((v) =>
-    v === "Petes kata"
-      ? (n += 10)
-      : v === "kata"
-      ? (n += 5)
-      : v === "eating"
-      ? (n += 1)
-      : null
-  );
-  if (n < 40) return "Super happy!";
-  if (n >= 40 && n < 70) return "Happy!";
-  if (n >= 70 && n < 100) return "Sad!";
-  if (n >= 100) return "Miserable!";
-}
+const babySharkLyrics = () => {
+  const fish = [
+    "Baby shark",
+    "Mommy shark",
+    "Daddy shark",
+    "Grandma shark",
+    "Grandpa shark",
+    "Let's go hunt",
+  ];
+  let lyrics = "";
+  let s = " doo".repeat(6);
+  fish.map((f) => {
+    lyrics += `${f},${s}\n`.repeat(3) + `${f}!\n`;
+  });
+  lyrics += "Run away,…";
+  return lyrics;
+};
