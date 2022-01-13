a1a8860950d66a400b776c572d092bb998ef2d0b
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-01-13 08:41:05 +0300

    add new kata

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function scrabbleScore(str) {
-  return str
-    .replace(/[^a-z]/gi, "")
-    .split("")
-    .reduce((a, b) => a + $dict[b.toUpperCase()] * 1, 0);
+function paul(x) {
+  let n = 0;
+  x.map((v) =>
+    v === "Petes kata"
+      ? (n += 10)
+      : v === "kata"
+      ? (n += 5)
+      : v === "eating"
+      ? (n += 1)
+      : null
+  );
+  if (n < 40) return "Super happy!";
+  if (n >= 40 && n < 70) return "Happy!";
+  if (n >= 70 && n < 100) return "Sad!";
+  if (n >= 100) return "Miserable!";
 }
