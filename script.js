263c8b4946ca8b3a75cbe2c08efd00b80a7caac6
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-01-15 17:24:59 +0300

    add new kata

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ class UserService {
     xhr.send();
     // xhr.responseText = "json";
     console.log(xhr);
-    const result = false;
+    let result = false;
     xhr.onload = function () {
       if (xhr.status !== "200") {
         result = xhr.repsonse;
@@ -35,16 +35,16 @@ class UserService {
     return result;
   }
 }
-// $("#login").click(function () {
-//   var username = $("#username");
-//   var password = $("#password");
-//   var res = new UserService(username, password).authenticate_user();
-//   if (res === true) {
-//     document.location.href = "/home";
-//   } else {
-//     alert(res.error);
-//   }
-// });
+$("#login").click(function () {
+  var username = $("#username");
+  var password = $("#password");
+  var res = new UserService(username, password).authenticate_user();
+  if (res === true) {
+    document.location.href = "/home";
+  } else {
+    alert(res.error);
+  }
+});
 document.querySelector("#login").addEventListener("click", () => {
   var res = new UserService("Вася", "898").authenticateUser("Вася", "898");
   if (res === true) {
@@ -53,3 +53,8 @@ document.querySelector("#login").addEventListener("click", () => {
     alert(res.error);
   }
 });
+const post = {
+  title: "My post",
+  likesQty: 5,
+};
+console.log(JSON.stringify(post));
