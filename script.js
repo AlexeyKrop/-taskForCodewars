998e23dd1a699b1330dcf07f0f90fe7753e59632
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-01-25 08:49:33 +0300

    add new kata

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 "use strict";
-const cannonsReady = (gunners) => {
-  return Object.values(gunners).every((response) => response === "aye")
-    ? "Fire!"
-    : "Shiver me timbers!";
-};
+const rps = (p1, p2) => {
+if (p1==='scissors'&&p2==='paper') return `Player 1 won!`;
+if (p1==='paper'&&p2==='rock') return `Player 1 won!`;
+if (p1==='rock'&&p2==='scissors') return `Player 1 won!`;
+if (p1==='paper'&&p2==='scissors') return `Player 2 won!`;
+if (p1==='rock'&&p2==='paper') return `Player 2 won!`;
+if (p1==='scissors'&&p2==='rock') return `Player 2 won!`;
+if (p1==='scissors'&&p2==='scissors') return `Draw!`;
+if (p1==='paper'&&p2==='paper') return `Draw!`;
+if
