aed48ba3c5e374d0c5bb6fab4e910beb07f600f6
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-02-01 09:56:35 +0300

    add new kata

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 "use strict";
-function sumTwoSmallestNumbers(numbers) {
-  let a = numbers.sort((a, b) => a - b);
-  return a[0] + a[1];
+function uglifyWord(s) {
+  s = s.toLowerCase().split("");
+  let flag = 1;
+  for (let i = 0; i < s.length; i++) {
+    if (/[a-z]/.test(s[i])) {
+      if (flag === 1) {
+        let str = s[i].toUpperCase();
+        s[i] = str;
+        flag = 0;
+      } else {
+        let str = s[i].toLowerCase();
+        s[i] = str;
+        flag = 1;
+      }
+    } else {
+      flag = 1;
+    }
+  }
+  return s.join(``);
 }
