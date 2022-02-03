84e84d96235ed32acbb94ddd794b6f84220a8408
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-02-03 09:37:57 +0300

    add new kata

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 "use strict";
-function uglifyWord(s) {
-  s = s.toLowerCase().split("");
-  let flag = 1;
-  for (let i = 0; i < s.length; i++) {
-    if (/[a-z]/.test(s[i])) {
-      if (flag === 1) {
-        let str = s[i].toUpperCase();
-        s[i] = str;
-        flag = 0;
-      } else {
-        let str = s[i].toLowerCase();
-        s[i] = str;
-        flag = 1;
-      }
-    } else {
-      flag = 1;
-    }
-  }
-  return s.join(``);
-}
+const cannonsReady = (gunners) => {
+  return Object.values(gunners).every((response) => response === "aye")
+    ? "Fire!"
+    : "Shiver me timbers!";
+};
