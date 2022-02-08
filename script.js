9a193bc39da93643d4acea74b7b9a29cce8ffcc3
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-02-08 13:23:42 +0300

    add newkata

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-function points(games) {
-  return games.reduce((previousValue, currentValue) => {
-    if (+currentValue[0] > +currentValue[2]) {
-      return (previousValue += 3);
-    } else if (currentValue[0] === currentValue[2]) {
-      return (previousValue += 1);
-    } else {
-      return (previousValue += 0);
-    }
-  }, 0);
+function pillars(num_pill, dist, width) {
+  if (num_pill > 1) {
+    return (num_pill - 1) * dist * 100 + width * (num_pill - 2);
+  } else {
+    return 0;
+  }
 }
-points(["1:0", "2:0", "3:0"]);
