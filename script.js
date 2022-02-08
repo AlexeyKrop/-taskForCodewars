298e0eb1d06a1c4b51cdc6e4066a91abda02ce95
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-02-08 09:00:22 +0300

    add new kata

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const cannonsReady = (gunners) => {
-  return Object.values(gunners).every((response) => response === "aye")
-    ? "Fire!"
-    : "Shiver me timbers!";
-};
+function pillars(num_pill, dist, width) {
+  // your code here
+  return num_pill > 1
+    ? (num_pill - 1) * dist * 100 + (num_pill - 2) * width
+    : 0;
+}
