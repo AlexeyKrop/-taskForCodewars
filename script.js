127d48937b9ebdc3ff8b00c875c042d3d2451f53
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-02-09 09:56:20 +0300

    add new kata

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-function pillars(num_pill, dist, width) {
-  if (num_pill > 1) {
-    return (num_pill - 1) * dist * 100 + width * (num_pill - 2);
-  } else {
-    return 0;
-  }
-}
+const nthEven = (n) => n * 2 - 2;
